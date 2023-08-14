c3f32c372cf78dff7cc95d69fdb49636f2a666e2
Mylo Cyrus <irahiggins740@gmail.com>
2023-08-14 08:49:05 -0700

    Add files via upload

---
+++ police_raid_room_search.py
@@ -0,0 +1,599 @@
+room search + police raid
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 38.75.42.130:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+1
+---------
+
+Pinging 38.75.42.130 with 32 bytes of data:
+Request timed out.
+Request timed out.
+Request timed out.
+Reply from 66.254.114.41: bytes=32 time=16ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=36ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=23ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 16ms, Maximum = 111ms, Average = 46ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=89ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=40ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=16ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=39ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 16ms, Maximum = 89ms, Average = 46ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=22ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=33ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=57ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=44ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 22ms, Maximum = 57ms, Average = 39ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=59ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=23ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=38ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=19ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 19ms, Maximum = 59ms, Average = 34ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=61ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=42ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=58ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=33ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 33ms, Maximum = 61ms, Average = 48ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=32ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=44ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=32ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=57ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 32ms, Maximum = 57ms, Average = 41ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=65ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=16ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=20ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=40ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 16ms, Maximum = 65ms, Average = 35ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=21ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=33ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=26ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=83ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 21ms, Maximum = 83ms, Average = 40ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=25ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=36ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=47ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=37ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 25ms, Maximum = 47ms, Average = 36ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=39ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=31ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=35ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=65ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 31ms, Maximum = 65ms, Average = 42ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=31ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=45ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=39ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=19ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 19ms, Maximum = 45ms, Average = 33ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=45ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=17ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=62ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=14ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 14ms, Maximum = 62ms, Average = 34ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=59ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=64ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=44ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=22ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 22ms, Maximum = 64ms, Average = 47ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=28ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=53ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=48ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=34ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 28ms, Maximum = 53ms, Average = 40ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=50ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=89ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=39ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=19ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 19ms, Maximum = 89ms, Average = 49ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=68ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=33ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=72ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=20ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 20ms, Maximum = 72ms, Average = 48ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=64ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=70ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=61ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=37ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 37ms, Maximum = 70ms, Average = 58ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=23ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=68ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=51ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=61ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 23ms, Maximum = 68ms, Average = 50ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=32ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=136ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=19ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=25ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 19ms, Maximum = 136ms, Average = 53ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=65ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=93ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=103ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=115ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 65ms, Maximum = 115ms, Average = 94ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=41ms TTL=54
+Request timed out.
+Reply from 66.254.114.41: bytes=32 time=19ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=47ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 3, Lost = 1 (25% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 19ms, Maximum = 47ms, Average = 35ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=68ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=47ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=27ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=42ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 27ms, Maximum = 68ms, Average = 46ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=40ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=42ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=43ms TTL=54
+Reply from 66.254.114.41: bytes=32 time=20ms TTL=54
+
+Ping statistics for 66.254.114.41:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 20ms, Maximum = 43ms, Average = 36ms
+0
+---------
+
+Pinging 66.254.114.41 with 32 bytes of data:
+Reply from 66.254.114.41: bytes=32 time=66ms TTL=54
