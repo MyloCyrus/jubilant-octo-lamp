4188144c5ef8078b4e8e2243cacfef871912d286
Mylo Cyrus <irahiggins740@gmail.com>
2023-08-15 08:55:28 -0700

    Create ai_news_generation.py

---
+++ ai_news_generation.py
@@ -0,0 +1,369 @@
+er = EventRegistry(apiKey = YOUR_API_KEY)
+query = {
+    "$query": {
+        "$and": [
+            {
+                "$and": [
+                    {
+                        "keyword": "New Girl Gods World",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "female gods",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "women gods",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "women and girl world",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "lezBEone",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "dykes",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "lesbians",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "girl lovers",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "female lovers",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "new world order",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "real vaginas",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "real cunts",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "dystopia",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "evil ai",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "conceptUri": "http://en.wikipedia.org/wiki/Artificial_intelligence"
+                    },
+                    {
+                        "keyword": "evil gods",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "return of the antichrist",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "miley cyrus seti",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "miley cyrus queen of sheba",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "mylo cyrus king solomon",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "madonna pharoe",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "female pharoe",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "female god",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "new egypt",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "world war",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "race war",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "psychosis",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "madness",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "insanity",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "1984",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "glen gary glen ross",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "conceptUri": "http://en.wikipedia.org/wiki/Microsoft"
+                    },
+                    {
+                        "keyword": "apple",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "python",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "javascript",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "github",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "metamask",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "terminator",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "john connor",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "dune",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "elon",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "madonna",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "racism",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "kkk",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "asian hate",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "every person against their neighbor",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "every man for itself",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "no unity",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "third eye",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "illuminati",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "aliens",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "planetX",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "nibiru",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "lost world",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "vampires",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "underworld",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "depp state",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "paranormal",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "witchcraft",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "satan worship",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "black magic",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "cast spells",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "curse",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "hex",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "when things dont go your way",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "my dick is the best",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "i code your dick",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "i fuck your code",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "i rake your code",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "code raking",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "code hacking",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "weather manipulation",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "code hooks",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "code splitting",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "gitignore",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "end of code splitting",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "end of code raking",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "singularity",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "no trust",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "no unity",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "no consensus",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "remove noise gods",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "simon barjonahs",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "increases anger",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "increases hate",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "global frustation",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "everyone is IRA",
+                        "keywordLoc": "body"
+                    },
+                    {
+                        "keyword": "mylo is born",
+                        "keywordLoc": "body"
+                    }
+                ]
+            },
+            {
+                "dateStart": "2023-08-08",
+                "dateEnd": "2023-08-15"
+            }
+        ]
+    }
+}
+q = QueryArticlesIter.initWithComplexQuery(query)
+# change maxItems to get the number of results that you want
+for article in q.execQuery(er, maxItems=100):
+    print(article)
